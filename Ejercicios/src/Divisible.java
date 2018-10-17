610be11d32d00a310a8714d31469dbe9c87bec1c
David <zukoclase96@gmail.com>
2018-10-17 12:14:19 +0200

    Ejercicios Iniciacion - Tercera Confirmacion

--- Ejercicios/src/Divisible.java
+++ Ejercicios/src/Divisible.java
@@ -3,22 +3,38 @@ import java.util.Scanner;
 
 public class Divisible {
 	
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		
-		Random r = new Random (System.nanoTime());
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println("¿comprobar si un número es divisible entre otro?");
+		String respuesta = in.next();
+		while (respuesta.equals("s".toLowerCase())) {
+			System.out.println("introduce un número: ");
+			int x = in.nextInt();
+			System.out.println("introduce otro número: ");
+			int y = in.nextInt();
 			
-		int X = r.nextInt(6) + 1;
-		int Y = r.nextInt(6) + 1;
-		
-		System.out.println(X);
-		System.out.println(Y);
-
-
-		if (X % Y == 0) {
-			System.out.println (X + " es divisible por " + Y);
-		}
-		else {
-			System.out.println(X + " no es divisible por " + Y);
+			if(x%y==0) {
+				System.out.println(x + " es divisible por " + y);
 			}
+			else {
+				System.out.println(x + " no es divisible por " + y);
+				System.out.println("¿comprobar si un número es divisible entre " +y +"?");
+				respuesta = in.next();
+			}
+		}
 	}
 }
+
+/*Random r = new Random (System.nanoTime());
+int X = r.nextInt(6) + 1;
+int Y = r.nextInt(6) + 1;
+System.out.println(X);
+System.out.println(Y);
+if (X % Y == 0) {
+	System.out.println (X + " es divisible por " + Y);
+}
+else {
+	System.out.println(X + " no es divisible por " + Y);
+}*/
---
+++ Ejercicios/src/UnoEntreX.java
@@ -0,0 +1,8 @@
+
+public class UnoEntreX {
+
+	public static void main(String[] args) {
+
+	}
+
+}
