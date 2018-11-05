4dc0d2b007e02b7bcfb84c7d3f15fd33d8fc9381
David <zukoclase96@gmail.com>
2018-11-05 13:47:30 +0100

    Ejercicios Iniciales - Sexta Confirmacion

---
+++ Ejercicios/src/DibujosConAsteriscos.java
@@ -0,0 +1,55 @@
+
+public class DibujosConAsteriscos {
+	 
+	static void volcan(int h) {
+		int asteriscos=2;
+		int espacios;
+		int total = (int)Math.pow(2, h)/2;	
+		for(int i=0; i<h; i++) {
+			espacios = total - asteriscos/2;
+			for(int j=0; j<espacios; j++) {			
+				System.out.print(' ');
+			}
+			for(int j=0; j<asteriscos; j++) {			
+				System.out.print('*');
+			}			
+			asteriscos += asteriscos;
+			System.out.println();
+		}
+	}
+	
+	static void mosaico(int f, int c) {
+		for(int i=0; i<f; i++) {
+			for(int j=0; j<c; j++) {
+				if((i % 2 == 0 && j % 2 !=0) || (i % 2 !=0 && j % 2==0)) {
+					System.out.print('*');
+				}
+				else {
+					System.out.print(' ');
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	static void tablero(int f, int c) {
+		for(int i=0; i<f; i++) {
+			for(int j=0; j<c; j++) {
+				if((i % 2 == 0 && j % 2 !=0) || (i % 2 !=0 && j % 2==0)) {
+					System.out.print('*');
+				}
+				else {
+					System.out.print(' ');
+				}
+			}
+			System.out.println();
+		}
+	}
+		
+	public static void main(String[] args) {
+	//	volcan(6);
+		mosaico(8,8);
+	//	tablero();
+	}
+
+}
---
+++ Ejercicios/src/PruebaDibujosConAsteriscos.java
@@ -0,0 +1,9 @@
+
+public class PruebaDibujosConAsteriscos {
+
+	public static void main(String[] args) {
+		DibujosConAsteriscos.volcan(6);
+		DibujosConAsteriscos.mosaico(8,8);
+	}
+
+}
