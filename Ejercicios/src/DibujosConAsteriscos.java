77d151cbd1fdb6ddedad16d09c482245b7fb74b2
David <zukoclase96@gmail.com>
2018-11-06 09:44:21 +0100

    Ejercicios Iniciales - Septima Confirmacion

--- Ejercicios/src/DibujosConAsteriscos.java
+++ Ejercicios/src/DibujosConAsteriscos.java
@@ -19,37 +19,51 @@ public class DibujosConAsteriscos {
 	}
 	
 	static void mosaico(int f, int c) {
-		for(int i=0; i<f; i++) {
-			for(int j=0; j<c; j++) {
-				if((i % 2 == 0 && j % 2 !=0) || (i % 2 !=0 && j % 2==0)) {
-					System.out.print('*');
-				}
-				else {
-					System.out.print(' ');
-				}
-			}
-			System.out.println();
-		}
-	}
-
-	static void tablero(int f, int c) {
-		for(int i=0; i<f; i++) {
-			for(int j=0; j<c; j++) {
-				if((i % 2 == 0 && j % 2 !=0) || (i % 2 !=0 && j % 2==0)) {
-					System.out.print('*');
-				}
-				else {
-					System.out.print(' ');
-				}
-			}
-			System.out.println();
-		}
-	}
 		
-	public static void main(String[] args) {
-	//	volcan(6);
-		mosaico(8,8);
-	//	tablero();
+		
+		for(int i=0; i<f; i++) {
+			for(int j=0; j<c; j++) {
+				if((i % 2 == 0 && j % 2 !=0) || (i % 2 !=0 && j % 2==0)) {
+					System.out.print('*');
+				}
+				else {
+					System.out.print(' ');
+				}
+			}
+			System.out.println();
+		}
 	}
 
+	static void tablero(int f, int c, int l) {
+		int ftxt = f*l;
+		int ctxt = c*l;
+		for(int i=0; i<ftxt; i++) {		
+			for(int j=0; j<ctxt; j++) {
+				int ctab = j / l;
+				int ftab = i / l;
+				if((ftab % 2 == 0 && ctab % 2 !=0) || (ftab % 2 !=0 && ctab % 2==0)) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}				
+			}
+			System.out.println();
+		}
+		
+		/*for(int i=0; i<f; i++) {
+			for(int j=0; j<l; j++) {
+				for (int z=0; z<c; z++) {
+					for(int r=0; r<l; r++)
+					if((i % 2 == 0 && z % 2 !=0) || (i % 2 !=0 && z % 2==0)) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		}*/
+	}
 }
--- Ejercicios/src/PruebaDibujosConAsteriscos.java
+++ Ejercicios/src/PruebaDibujosConAsteriscos.java
@@ -4,6 +4,6 @@ public class PruebaDibujosConAsteriscos {
 	public static void main(String[] args) {
 		DibujosConAsteriscos.volcan(6);
 		DibujosConAsteriscos.mosaico(8,8);
+		DibujosConAsteriscos.tablero(10,10,4);
 	}
-
 }
